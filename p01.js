c3bafcf17262313f11c2d6a3a9b7c74ceb072b83
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-08-29 11:15:48 +0600

    Time added

--- p01.js
+++ p01.js
@@ -12,3 +12,38 @@ var day = toDay.getDay();
 var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 console.log("Today is : " + dayList[day]);
+
+var hour = toDay.getHours();
+var minutes = toDay.getMinutes();
+var second = toDay.getSeconds();
+
+var prepend = (hour >= 12) ? "PM" : "AM";
+
+// convert 24 hour into 12 
+hour = (hour >= 12)? hour - 12 : hour;
+
+
+// check special Case when hour is 0 
+if(hour === 0 && prepend === "PM"){
+    if(minutes === 0 && second === 0){
+        hour = 12;
+        prepend = "noon";
+    }else{
+        hour = 12;
+        prepend = "PM";
+    }
+}
+
+
+// check special Case when hour is 0
+if(hour === 0 && prepend === "AM"){
+    if(minutes === 0 && second === 0){
+        hour = 12;
+        prepend = "MidNight";
+    }else{
+        hour = 12;
+        prepend = "AM";
+    }
+}
+
+console.log("Current Time is: "+ hour + " : " + minutes + " : " + second + " " +prepend);
