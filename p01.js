d04f01288ed7ffc986fbf040c9c6676f0b1e9d9f
SudiptaRoy05 <sudiptaroyballave121@gmail.com>
2024-08-29 11:05:07 +0600

    day in naw available

--- p01.js
+++ p01.js
@@ -1,1 +1,14 @@
-console.log("Problem Solving")
+/**
+ * 1. Write a JavaScript program to display the current day and time in the following format. 
+ * Sample Output : Today is : Tuesday.
+ * Current time is : 10 PM : 30 : 38
+*/
+
+var toDay = new Date();
+
+var day = toDay.getDay();
+// console.log(day)
+
+var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+console.log("Today is : " + dayList[day]);
